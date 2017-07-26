fc0c8728cc3a4220ce64dbbf12f3d9f7d83f8f29
chi-feng <chifeng@mit.edu>
2017-07-26 16:06:33 -0400

    Initial commit

---
+++ autocomplete_server.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+import tornado.web
+import json
+
+wordlist = []
+with open('sorted.txt') as f:
+    wordlist = f.read().splitlines()
+    wordlist = [word.lower() for word in wordlist]
+
+max_suggestions = 10
+
+class AutocompleteHandler(tornado.web.RequestHandler):
+    def post(self):
+        self.set_header("Content-Type", "application/javascript")
+        prefix = self.get_body_argument('search_string');
+        suggestions = [word for word in wordlist if word.startswith(prefix)]
+        if len(suggestions) > max_suggestions:
+            suggestions = suggestions[0:max_suggestions]
+        self.write(json.dumps(suggestions))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/autocomplete", AutocompleteHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
